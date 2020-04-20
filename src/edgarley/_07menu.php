715c3dbc3884f3d9a426dc37f0f66ecef776d3a2
Bart Nagel <bart@tremby.net>
2020-04-20 03:11:52 +0100

    Remove unused variable

--- src/edgarley/_07menu.php
+++ src/edgarley/_07menu.php
@@ -2,8 +2,6 @@
 <ul id="list-nav">
 <li> </li>
 <?php
-  global $got_j ;
-  $got_j = basename($_SERVER['PHP_SELF']);     // current_filename
   menuitem("Text",        "text");
   menuitem("Family tree", "famtree.png", "_new");
   menuitem("Sources",     "acks");
--- src/montie/_007menu.php
+++ src/montie/_007menu.php
@@ -2,8 +2,6 @@
 <ul id="list-nav">
 <li> </li>
 <?php
-  global $got_j ;
-  $got_j = basename($_SERVER['PHP_SELF']);     // current_filename
   menuitem("Section 1",   "1");
   menuitem("Section 2",   "2");
   menuitem("Family tree", "famtree.png", "_new");
