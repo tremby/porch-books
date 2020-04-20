055175e90756a661bddf4ed8afb0d185e241493c
Bart Nagel <bart@tremby.net>
2020-04-20 17:21:05 +0100

    Improve links from home page to books

--- src/index.php
+++ src/index.php
@@ -6,13 +6,13 @@
 
 <box style="background-color:#dfd">
 
-<a href="edgarley/text">
+<a href="edgarley/text" aria-label="Read Victorian Edgarley: the fall of the House of Porch">
 <?php   fig("left","porch-edgarley-cover.jpg", .75, 1)   ?>
 Click for web edition
 <?php   figend()   ?>
 </a>
 
-<a href="montie/1">
+<a href="montie/1" aria-label="Read Montie Porch, a charmed life: the Glastonbury man who married Winston Churchill’s mother">
 <?php   fig("right","porch-montie-cover.jpg", .75, 1)   ?>
 Click for web edition
 <?php   figend()   ?>
@@ -29,9 +29,9 @@ The Porches’ own estate at Edgarley comprised a fine house and a fascinating f
 <p>Infamy and downfall came in the final years of the century when a Porch heiress was convicted of the murder of her husband, making international headlines at the time.
 Within twenty years, their name damaged beyond redemption, the Porches had left Edgarley, never to return.</p>
 <p>Roger Parsons has researched the family records during his time as archivist at Millfield Prep School, owners of the Edgarley Estate since 1945.</p>
-<p>His <strong><cite>Victorian Edgarley: the fall of the House of Porch</cite></strong> was published by Abbey Press in 2010, followed by a second edition in 2014, now available online, above.</p>
+<p>His <strong><cite>Victorian Edgarley: the fall of the House of Porch</cite></strong> was published by <a href="http://abbeypress.net/">Abbey Press</a> in 2010, followed by a second edition in 2014, <a href="edgarley/text" aria-label="Read Victorian Edgarley: the fall of the House of Porch">now available online</a>.</p>
 <p>A matching booklet — <strong><cite>Montie Porch, a charmed life: the Glastonbury man who married Winston Churchill’s mother</cite></strong> — was also published by Abbey Press in 2012 and reprinted in 2015.
-A second edition in 2019 is now also available online, above.</p>
+A second edition in 2019 is now also <a href="montie/1" aria-label="Read Montie Porch, a charmed life: the Glastonbury man who married Winston Churchill’s mother">available online</a>.</p>
 <p>Roger would like to pay tribute to his friend, Jim Nagel, founder of Abbey Press, who passed away in March 2020 before he was able to complete the task of making the Victorian Edgarley story available on the web.
 Jim was a man whose love of the unique place that is Glastonbury shone through everything he did and achieved, and he will be sorely missed.</p>
 <p>Roger is immensely grateful to Jim’s son, Bart Nagel, for skilfully and swiftly finishing what his father sadly could not.</p>
