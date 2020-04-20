1be316ed7a6b86f2c39c677f220edd9cd9a3cd6e
Bart Nagel <bart@tremby.net>
2020-04-20 02:49:43 +0100

    Add acknowledgements from file sent by Roger
    File: Victorian Edgarley  - sources for online version FINAL.docx

--- src/edgarley/acks.php
+++ src/edgarley/acks.php
@@ -26,6 +26,21 @@
 
 <p>As regards other sources for the booklet, I have consulted the National Records Office in Kew, Somerset County Records Office and Studies Library in Taunton, contemporary editions of the <i>West of England Advertiser</i> and the <i>Central Somerset Gazette</i>, Armynell Goodall&rsquo;s book on the Bulleids of Glastonbury and the book on Stuckey&rsquo;s Bank by P.&nbsp;T.&nbsp;Saunders.</p>
 
+<p>Thanks for their help in revealing the story of the Porch family of Edgarley are due to:</p>
+<ul>
+  <li>Simon Fuller, late Head of History at Millfield Prep School, for some splendid groundwork in uncovering the first details about the history of the Edgarley Estate.</li>
+  <li>John Knee, teacher at Malvern College and former Edgarley pupil, who alerted the author to the Malvern College connection with Robert Bagehot Porch and his son, Alec.</li>
+  <li>Alec Porch, who was most generous in his sharing of his copious notes about the Porch family.</li>
+  <li>Richard J Porch, one of Albert’s great-grandsons, for his encouragement, friendship and support.</li>
+  <li>Sally Melia, great-granddaughter of Albert’s younger brother, Reginald, and great-niece of Montie Porch, who provided a number of family photos and scrapbooks of family information.</li>
+  <li>Janita Clamp for the loan of all the notes and photos she collected in the 1980s for her father, Professor Clamp of Bristol University, on behalf of Professor Tokuoka of Yokohama, author of the original Japanese version of the book Murder on the Bluff, which was translated and adapted for publication in England by Molly Whittington-Egan.</li>
+  <li>Julian Wood, the recently revealed great-great-great-grandson of Albert Porch.</li>
+  <li>Dr Dick Shilton, archivist of Millfield Senior School, for his expert, invaluable help in digitising and improving the quality of many of the photos.</li>
+  <li>Dr Tim Hopkinson-Ball, Paul Ashdown and Dr Adam Stout for continuing to keep their eye out for references to the Porch family in their own research.</li>
+  <li>Ron Treloggan, Glastonbury Cemetery</li>
+</ul>
+<p>Other sources of information used were the National Records Office in Kew, the Somerset County Records Office and Studies Library in Taunton, contemporary editions of the West of England Advertiser and the Central Somerset Gazette, Armynell Goodall’s book on The Bulleids of Glastonbury, the book on Stuckey’s Bank by PT Saunders, and Ralph G Martin’s biography of Lady Randolph Churchill, wife of Montagu Porch.</p>
+
 <br>
 <p style:"text-align:center">&mdash; <b>Roger Parsons</b>, Glastonbury, 2010</p>
 
