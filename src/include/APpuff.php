5c5a5cba061f5cc51103737b98743c062b0b804e
Bart Nagel <bart@tremby.net>
2020-04-22 17:56:06 +0100

    Add Roger's email address to footer

--- src/include/APpuff.php
+++ src/include/APpuff.php
@@ -1,4 +1,6 @@
 
 <center>
+<a href="mailto:parsons.r@millfieldprep.com">parsons.r@millfieldprep.com</a>
+
 <img src="/include/APlogo25.png" alt="Abbey Press Glastonbury" width=143 height=36>
 </center>
