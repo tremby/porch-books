ea3f63f42712b49b78335036ac2af6ae2dbd9b6a
Bart Nagel <bart@tremby.net>
2020-04-22 18:10:14 +0100

    Add missing contents file (oops)

---
+++ src/montie/contents.php
@@ -0,0 +1,54 @@
+<?php
+  $titlebar = "Contents &mdash; Montie Porch: a charmed life";
+  $notedir  = basename(dirname(__FILE__));
+  include '../include/articleheadporch.php';
+?>
+
+<?php include '_005title.php'; ?>
+<?php include '_007menu.php'; ?>
+<br>
+
+<p><strong><a href="1">Begin reading with section 1</a></strong></p>
+
+<h2>Contents</h2>
+
+<nav style="columns: 2; column-gap: 2em;">
+	<section style="break-inside: avoid;">
+		<h3>Section 1</h3>
+		<ul>
+			<li><a href="1#a">A life of contrasts</a></li>
+			<li><a href="1#b">Sources</a></li>
+			<li><a href="1#c">Early years</a></li>
+			<li><a href="1#d">Immediate family</a></li>
+			<li><a href="1#e">The absent father</a></li>
+			<li><a href="1#f">School and Oxford</a></li>
+			<li><a href="1#g">The Boer War</a></li>
+			<li><a href="1#h">Archaeology in Sinai</a></li>
+			<li><a href="1#i">West Africa</a></li>
+			<li><a href="1#j">Jennie</a></li>
+			<li style="margin-left: 1em;"><a href="1#k">A piano footnote</a></li>
+			<li><a href="1#l">The Great War</a></li>
+		</ul>
+	</section>
+
+	<section>
+		<h3>Section 2</h3>
+		<ul>
+			<li><a href="2#m">Engagement and wedding</a></li>
+			<li><a href="2#n">Trouble on all fronts</a></li>
+			<li><a href="2#o">Public glare in London</a></li>
+			<li><a href="2#p">The Gold Coast and tragedy</a></li>
+			<li><a href="2#q">Empty house</a></li>
+			<li><a href="2#r">Marriage in Italy</a></li>
+			<li><a href="2#s">Return to Glastonbury</a></li>
+			<li><a href="2#t">From Lodge to lodger</a></li>
+			<li><a href="2#u">In the papers</a></li>
+			<li><a href="2#v">Glastonbury&rsquo;s last Porches</a></li>
+			<li><a href="2#w">Montie&rsquo;s destiny</a></li>
+		</ul>
+	</section>
+</nav>
+
+<?php
+  include '../include/articletail.php';
+?>
