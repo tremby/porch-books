fa3285dce79523d6562b7596ac3aafc45e769f06
Bart Nagel <bart@tremby.net>
2020-04-22 18:21:27 +0100

    Adjust alignment of logo, make email address italic

--- src/include/APpuff.php
+++ src/include/APpuff.php
@@ -1,10 +1,10 @@
 
 <center>
-	<small>
+	<small style="font-style: italic;">
 		<a href="mailto:parsons.r@millfieldprep.com">parsons.r@millfieldprep.com</a>
 	</small>
 
 	&emsp;
 
-	<img src="/include/APlogo25.png" alt="Abbey Press Glastonbury" width=143 height=36 align=middle>
+	<img src="/include/APlogo25.png" alt="Abbey Press Glastonbury" width=143 height=36 style="vertical-align: middle;">
 </center>
